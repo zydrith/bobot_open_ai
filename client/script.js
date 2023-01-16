95698c80640842007f804127bfee8686837555e1
zydrith <zeeharith@gmail.com>
2023-01-16 17:31:36 +0800

    finalized

--- client/script.js
+++ client/script.js
@@ -77,7 +77,7 @@ const handleSubmit = async (e) => {
 
   loader(messageDiv);
 
-  const response = await fetch('http://localhost:5000', {
+  const response = await fetch('https://bobot.onrender.com', {
     method: 'POST',
     headers: {
       'Content-Type': 'application/json'
